66502282192b9847de5f8b6fa0891a11f80252fa
swso0530 <swso0530@outlook.kr>
2026-03-31 16:36:15 +0900

    오류 수정

--- SQL/LeetCode/easy/1517.sql
+++ SQL/LeetCode/easy/1517.sql
@@ -3,4 +3,5 @@ SELECT
     name,
     mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
+  AND BINARY RIGHT(mail, 13) = '@leetcode.com';
